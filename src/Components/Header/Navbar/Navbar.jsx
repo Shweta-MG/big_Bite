3ccbae017c266d97ee4c83751a11cdb05fe872e5
Shweta <shweta.malav@gmail.com>
2024-01-06 23:23:09 +0100

    Initial commit

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar_Container">
+      <div className="navbar_logo">Blanquette</div>
+      <div className="navbar_list_container">
+        <ul>
+          <li>Home</li>
+          <li>Pages</li>
+          <li>Blog</li>
+          <li>Shop</li>
+        </ul>
+      </div>
+      <div className="navbar_cart">Cart</div>
+    </div>
+  );
+};
+
+export default Navbar;
